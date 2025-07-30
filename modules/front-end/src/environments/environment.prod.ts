847b941706941d318cd0fd35e0d38b6794c63c6a
jcanadilla-gibobs <82891060+jcanadilla-gibobs@users.noreply.github.com>
2025-07-30 16:33:25 +0200

    Update environment.prod.ts

--- modules/front-end/src/environments/environment.prod.ts
+++ modules/front-end/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: window['env']['apiUrl'] || location.origin.replace(/\/$/, ''),
+  url: window['env']['apiUrl'] || 'https://featbit-api.allbanks.es',
   demoUrl: window['env']['demoUrl'] || 'https://featbit-samples.vercel.app',
-  evaluationUrl: window['env']['evaluationUrl'] || location.origin.replace(/\/$/, '')
+  evaluationUrl: window['env']['evaluationUrl'] || 'https://featbit-evaluation.allbanks.es'
 };
